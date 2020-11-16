3495edf51abf851f743337e2167ff24969cc1008
Lars Hanegraaf <l.j.t.hanegraaf@student.tue.nl>
2020-11-16 13:36:06 +0100

    add STL decomposition

--- fclearn/decomposition.py
+++ fclearn/decomposition.py
@@ -1,20 +1,31 @@
 """Decomposition methods as a Scikit-learn transformer."""
+import pandas as pd
+import statsmodels.api as sm
 from sklearn.base import BaseEstimator, TransformerMixin
 from statsmodels.tsa.seasonal import STL
 
 
-class STLTransformer(BaseEstimator, TransformerMixin):
-    """STL Decomposition."""
+class DecomposeTransformer(BaseEstimator, TransformerMixin):
+    """Decomposition transformer for classical and STL Decomposition."""
 
-    def __init__(self):
+    def __init__(self, method):
         """Constructor."""
-        pass
+        self.method = method
 
     def fit(self, X, y=None):
         """Fit the transformer."""
-        self.stl_results = STL(X).fit()
+        if self.method == "stl":
+            self.decompose_results = STL(X.values, period=52, seasonal=13).fit()
+        if self.method == "classic":
+            self.decompose_results = sm.tsa.seasonal_decompose(
+                X.values, model="additive", period=52, extrapolate_trend=True
+            )
         return self
 
     def transform(self, X):
         """Transform the series into trend, seasonal and remainder."""
-        return X
+        X_ = pd.DataFrame(index=X.index)
+        X_["trend"] = self.decompose_results.trend
+        X_["seasonal"] = self.decompose_results.seasonal
+        X_["error"] = self.decompose_results.resid
+        return X_
